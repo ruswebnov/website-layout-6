8ba213ec76ce1f7abf74e575a097724622f3855f
Ruslan <ruslantrade2019@gmail.com>
2020-07-31 13:07:52 +0700

    ready demo version desktop

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+ $('.results__inner').slick({
+  slidesToShow: 6,
+  slidesToScroll: 6
+ });
+
+ $('.reviews__inner').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+ });
+
+})
