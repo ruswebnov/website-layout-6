c147582a2fe24620b7e241aa66ac9f96ea36728b
Ruslan <ruslantrade2019@gmail.com>
2020-08-02 00:02:18 +0700

    ready pre laptop and mobile version

--- js/main.js
+++ js/main.js
@@ -1,13 +1,92 @@
 $(document).ready(function() {
 
+
  $('.results__inner').slick({
-  slidesToShow: 6,
-  slidesToScroll: 6
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  centerMode: true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 544,
+      settings: {
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
  });
+
+
 
  $('.reviews__inner').slick({
   slidesToShow: 2,
   slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+  ]
  });
 
-})
+
+
+ $('.uniq__wrapper').slick({
+   slidesToShow: 6,
+   slidesToScroll: 6,
+   autoplay: true,
+   autoplaySpeed: 3000,
+   arrows: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+      centerPadding: '60px',
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 544,
+      settings: {
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+ });
+
+
+
+});
